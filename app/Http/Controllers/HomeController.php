ac25491da7ed74afb659c9c73195c8f3403b0a48
h3x4crypt <theworldeye1@gmail.com>
2018-07-30 14:29:16 +0300

    0x05

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 use Auth;
@@ -8,7 +8,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-       
+           	if (Auth::check()) {
+    		$statuses = Status::notReply()->where(function($query) {
+    			return $query->where('user_id', Auth::user()->id)->orWhereIn('user_id', Auth::user()->friends()->lists('id') );
+    		})
+    		->orderBy('created_at', 'desc')->paginate(10);
+    		return view('timeline.index')->with('statuses', $statuses);
+    	}
 
         return view('home');
     }
--- resources/views/user/partials/userblock.blade.php
+++ resources/views/user/partials/userblock.blade.php
@@ -1,12 +1,12 @@
 <div class="media">
 	<a class="pull-left" href="{{ route('profile.index', ['username' => $user->username]) }}">
-	<img class="media-object" alt="{{ $user->getNameOrUsername() }}" src="/Async/public/images/uploads/avatars/{{ $user->avatar }}" style="width:40px; height:40px; float:left; border-radius:50%; margin-right:25px;">
+	<img class="media-object" alt="{{ $user->getNameOrUsername() }}" src="/h3x4/public/images/uploads/avatars/{{ $user->avatar }}" style="width:40px; height:40px; float:left; border-radius:50%; margin-right:25px;">
 	</a>
 	<div class="media-body">
 		<h4 class="media-heading"><a href="{{ route('profile.index', ['username' => $user->username]) }}">@<!---->{{ $user->getNameOrUsername() }}</a> <span style="font-size:12px; color: gray;">| {{ $user->getName() }}</span></h4>
 		@if ($user->location)
 			<p>{{ $user->location }}</p>
 		@endif
-		
+
 	</div>
 </div>
