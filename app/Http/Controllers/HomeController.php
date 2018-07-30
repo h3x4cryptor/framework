c95325cbe13eee4a50dff9dba2ccf45bca3ce7fd
h3x4crypt <theworldeye1@gmail.com>
2018-07-30 15:10:06 +0300

    migrations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,13 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-           	if (Auth::check()) {
-    		$statuses = Status::notReply()->where(function($query) {
-    			return $query->where('user_id', Auth::user()->id)->orWhereIn('user_id', Auth::user()->friends()->lists('id') );
-    		})
-    		->orderBy('created_at', 'desc')->paginate(10);
-    		return view('timeline.index')->with('statuses', $statuses);
-    	}
 
         return view('home');
     }
